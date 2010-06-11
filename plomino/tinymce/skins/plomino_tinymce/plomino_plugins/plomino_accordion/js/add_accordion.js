926155b0f7a90382eb8c0cfe3c0b05b5dcf2abc4
leprodupure <leprodupure@39f4f2d6-6c1e-0410-acfa-ed0e10f9290e>
2010-06-11 09:06:41 +0000

    Accordions can be added recursively (until 4 sub-sections).

--- plomino/tinymce/skins/plomino_tinymce/plomino_plugins/plomino_accordion/js/add_accordion.js
+++ plomino/tinymce/skins/plomino_tinymce/plomino_plugins/plomino_accordion/js/add_accordion.js
@@ -2,9 +2,23 @@
 function insert_accordion() {
 	var ed = tinyMCEPopup.editor;
 	tinyMCEPopup.restoreSelection();
-	var title = document.getElementById("title").value;
-	var zone = '<h3 class="plomino-accordion-header">' + title + '</h3>'
-				+ '<div class="plomino-accordion-content">' + ed.selection.getContent() + '</div>';
-	ed.execCommand('mceInsertContent', false, zone, {skip_undo : 1});
+	var selection = ed.selection.getContent();
+
+	var node = ed.selection.getNode();
+	
+	var nbAncestors = 3 + tinymce.DOM.getParents(node, "div.plomino-accordion-content").length;
+	
+	if (nbAncestors <= 6)
+	{
+		var title = document.getElementById("title").value;
+		var zone = '<h' + nbAncestors + ' class="plomino-accordion-header"><a href="#">' + title + '</a></h' + nbAncestors + '>'
+					+ '<div class="plomino-accordion-content">' + selection + '</div>';
+		ed.execCommand('mceInsertContent', false, zone, {skip_undo : 1});
+	}
+	else
+	{
+		alert("Too many recursive sections.");
+	}
+	
 	tinyMCEPopup.close();
 }

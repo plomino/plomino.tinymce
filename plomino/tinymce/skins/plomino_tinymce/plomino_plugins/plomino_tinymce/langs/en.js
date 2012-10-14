f9e663c89b4a7cb4ff09bd5007f7b8c33bd16205
Alessandro Pisa <alessandro.pisa@redturtle.it>
2012-10-14 10:58:04 +0200

    moved tinymce plugins to resourceRegistries

--- plomino/tinymce/skins/plomino_tinymce/plomino_plugins/plomino_tinymce/langs/en.js
+++
@@ -1,8 +0,0 @@
-tinyMCE.addI18n({en:{
-plomino_tinymce:{
-field:"Add/edit a Plomino Field",
-action:"Add/edit a Plomino Action",
-subform:"Add a Plomino Sub-form",
-hidewhen:"Add a Plomino Hiden-when zone",
-edition_forbidden:"Please save the form before using this button."
-}}});
--- plomino/tinymce/skins/plomino_tinymce/plomino_plugins/plomino_tinymce/langs/fr.js
+++
@@ -1,8 +0,0 @@
-tinyMCE.addI18n({fr:{
-plomino_tinymce:{
-field:"Ajouter/éditer un champ Plomino",
-action:"Ajouter/éditer une action Plomino",
-subform:"Ajouter/éditer un sous-formulaire Plomino",
-hidewhen:"Ajouter/éditer une zone « Cacher quand… » Plomino",
-edition_forbidden:"Vous devez sauvegarder ce formulaire avant d'utiliser ce bouton."
-}}});

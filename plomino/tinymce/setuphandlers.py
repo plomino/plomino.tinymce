57e0c0a605404a268aa3b7f14dcf35e4b63beb20
Eric BREHAULT <ebrehault@gmail.com>
2012-12-14 16:33:40 +0100

    fix generic setup to preserve existing plugins definition

--- plomino/tinymce/setuphandlers.py
+++
@@ -1,6 +0,0 @@
-def postInstall(context):
-    """Called as at the end of the setup process. """
-    
-    site = context.getSite()
-    if hasattr(site, "portal_tinymce"):
-        site.portal_tinymce.customplugins = '\n'.join((line for line in site.portal_tinymce.customplugins.splitlines() if line != "plominofield|/plomino_plugins/plominofield/editor_plugin.js"))

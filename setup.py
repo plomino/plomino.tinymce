ddb0d682bf7d3815ee9060be8a715c0df24307fc
Silvio Tomatis <silviot@gmail.com>
2013-06-21 15:28:43 +0200

    README.txt --> README.rst for better github project frontpage

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ version = '0.7.3'
 setup(name='plomino.tinymce',
       version=version,
       description="TinyMCE Plomino Embedded Plugin",
-      long_description=open("README.txt").read() + "\n" +
+      long_description=open("README.rst").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from
       # http://pypi.python.org/pypi?%3Aaction=list_classifiers
